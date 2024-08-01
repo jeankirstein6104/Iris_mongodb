428ee00bc26e64aa6a24b5ac0fe8a0bee16060d8
jeankirstein6104 <jeankirstein6104@gmail.com>
2024-08-01 20:30:55 +0530

    Update url.py

--- structure/url.py
+++ structure/url.py
@@ -1,11 +1,11 @@
 import os
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
 
 # Read the MongoDB URI from the environment variable
 MONGODB_URI = 'mongodb+srv://jeankirstein6104:UaSXsPMMz99Acj9v@basics-projects.buat5ge.mongodb.net/?retryWrites=true&w=majority&appName=basics-projects'
 
-client = MongoClient(MONGODB_URI)
+client = AsyncIOMotorClient(MONGODB_URI)
 
 db = client.flowers
 
