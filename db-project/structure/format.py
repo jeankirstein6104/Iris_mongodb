3a0fd0bf9621327a6a92ca1043a75e8024c0c4f4
jeankirstein6104 <jeankirstein6104@gmail.com>
2024-08-01 17:38:06 +0530

    Delete db-project directory

--- db-project/structure/format.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-# class created to perform data validation and to reject invalid data
-class Predict(BaseModel):
-    sepal_length_cm: float
-    sepal_width_cm: float
-    petal_length_cm: float
-    petal_width_cm: float
